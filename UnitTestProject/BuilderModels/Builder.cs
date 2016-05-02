cda67075b1f72f464ff631a4c3b7224624bafcb5
Lucas Madeira Zamecki <zameck1@hotmail.com>
2016-05-01 22:48:09 -0400

    Criando projeto de teste, chamando controller pelo metodo de teste

---
+++ UnitTestProject/BuilderModels/Builder.cs
@@ -0,0 +1,26 @@
+﻿using GroupUpWeb.Helpers.Uow;
+using GroupUpWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.BuilderModels
+{
+    public class Builder
+    {
+        public static User user(IUnityOfWork Uow)
+        {
+            User user = new User(Uow);
+            user.Name = "NameGeneratedUser1";
+            user.LastName = "LastNameGeneratedUser1";
+            user.Email = "EmailGeneratedUser1@email.com";
+            user.Password = "PasswordGeneratedUser1";
+            user.Token = "HashKeyGeneratedUser1";
+            user.SigninDate = DateTime.Parse("2009-12-05");
+            user.LastLogin = DateTime.Parse("2016-02-05");
+            return user;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroupUpWeb.Controllers;
+using GroupUpWeb.BusinessInterface;
+using GroupUpWeb.Models;
+using GroupUpWeb.Helpers.Uow;
+using GroupUpWeb.Helpers.EF;
+using UnitTestProject.BuilderModels;
+using System.Net;
+using GroupUpWeb.Helpers;
+using Moq;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        static Mock<GroupUpContext> dbcontext;
+
+        static Mock<RepositoryFactories> repositoryFactory;
+
+        static Mock<IRepositoryProvider> repositoryProvider;
+
+        static Mock<UnityOfWork> Uow;
+
+        static Mock<User> userInterface;
+
+        static Mock<UserFriends> userFriendsInterface;
+
+        static Mock<UsersController> controller;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext) {
+            dbcontext = new Mock<GroupUpContext>();
+
+            dbcontext.Setup(x => x.Set<User>());
+
+            repositoryFactory = new Mock<RepositoryFactories>();
+
+            repositoryProvider = new Mock<IRepositoryProvider>();
+
+            repositoryProvider.Setup(x => x.GetRepositoryForEntityType<User>());
+
+            Uow = new Mock<UnityOfWork>(repositoryProvider.Object);
+
+            userInterface = new Mock<User>(Uow.Object);
+
+            userFriendsInterface = new Mock<UserFriends>(Uow.Object);
+
+            controller = new Mock<UsersController>(userInterface.Object, userFriendsInterface.Object);
+
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {           
+            var result = controller.Object.CreateUser(Builder.user(Uow.Object));
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
